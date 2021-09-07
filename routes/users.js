f99015f56f3a5420c9d36526772108891bbe6d82
Danyyl <danyyl.con@gmail.com>
2021-09-07 16:22:44 -0400

    delete returnTo url from session

--- routes/users.js
+++ routes/users.js
@@ -31,6 +31,7 @@ router.get('/login', (req, res) => {
 router.post('/login', passport.authenticate('local', {failureFlash: true, failureRedirect: '/login'}), (req, res) => {
     req.flash('success', 'Welcome Back!');
     const redirectUrl = req.session.returnTo || '/campgrounds';
+    delete req.session.returnTo;
     res.redirect(redirectUrl);
 });
 
